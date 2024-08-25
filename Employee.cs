1a9d05fc32e4088b3a19c308dcdb5881dad35501
Shrikant Pawar <shrikantpawar703@gmail.com>
2024-08-25 02:10:58 -0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedDemo
+{
+	internal class Employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+
+		public void GetData()
+		{
+			Console.WriteLine("Enter Employee Id");
+			this.Id = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter Employee Name");
+			this.Name = Console.ReadLine();
+
+			Console.WriteLine("Enter Employee Adress");
+			this.Address = Console.ReadLine();
+		}
+
+		public void PutData()
+		{
+			Console.WriteLine($"Employee Id = {this.Id}");
+			Console.WriteLine($"Employee Name = {this.Name}");
+			Console.WriteLine($"Employee Adress = {this.Address}");
+
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using ObjectOrientedDemo;
+
+class Program() { 
+public static void Main()
+{
+		//CreateEmployee();
+
+		Employee employee = new Employee();
+		employee.GetData();
+		employee.PutData();
+
+
+	}
+
+	public static void CreateEmployee()
+	{
+		Employee employee = new Employee();
+		employee.Id = 10;
+		employee.Name = "Array";
+		employee.Address = "Ahemdabad";
+
+		Console.WriteLine($"Employee Id = {employee.Id}");
+		Console.WriteLine($"Employee Name = {employee.Name}");
+		Console.WriteLine($"Employee Adress = {employee.Address}");
+
+		Console.WriteLine("-----------------------------------------------------------------");
+
+		Employee employee1 = new Employee();
+		employee1.Id = 10;
+		employee1.Name = "String";
+		employee1.Address = "Surat";
+
+		Console.WriteLine($"Employee1 Id = {employee1.Id}");
+		Console.WriteLine($"Employee1 Name = {employee1.Name}");
+		Console.WriteLine($"Employee1 Adress = {employee1.Address}");
+	}
+
+}
